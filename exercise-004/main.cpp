f75c27c6eeb5ab48c116afea4425bd4bf508e280
janu000 <149794196+janu000@users.noreply.github.com>
2023-11-24 15:10:21 +0100

    .

--- exercise-004/main.cpp
+++ exercise-004/main.cpp
@@ -3,9 +3,11 @@
 
 #include "CLI/CLI.hpp"
 #include "config.h"
+#include "bmp.h"
 
 auto main(int argc, char **argv) -> int
 {
+    std::string fname{DEFAULT_BMP_FILE};
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -14,13 +16,13 @@ auto main(int argc, char **argv) -> int
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        //app.add_option("-f, file", fname, fmt::print("The filename for the Bitmap, "));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
-
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
@@ -29,6 +31,8 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello, {}!\n", app.get_name());
 
     /* INSERT YOUR CODE HERE */
-
+    BMP DHBWlogo = BMP(fname);
+    fmt::print("file size: {}\n", DHBWlogo.info_header.size);
+    DHBWlogo.write();
     return 0; /* exit gracefully*/
 }
